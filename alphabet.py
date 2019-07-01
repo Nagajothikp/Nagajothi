a527b01e9067a2e90025c0bd8c71b6ca7175ccd3
unknown <kpnagajothi1999@gmail.com>
2019-07-01 20:21:18 +0530

    'fddf'

---
+++ alphabet.py
@@ -0,0 +1,5 @@
+ch=input("Enter a character:")
+if((ch>='a' and ch<='z' ) or (ch>='A' and ch<='Z' )):
+    print("The given character ",ch,"is a Alphabet")
+else:
+    print("The given character ",ch,"is not a Alphabet")
