2b114e20ecc15c3e3ee2fc656802e148015fe873
unknown <kpnagajothi1999@gmail.com>
2019-07-01 14:03:33 +0530

    'fdfd'

---
+++ vowel.py
@@ -0,0 +1,6 @@
+char=input("Enter the character:")
+if(char=='a' or char=='A' or char=='e' or char=='E' or char=='i' or char=='I'
+ or char=='o' or char=='O' or char=='u' or char=='U' ):
+    print(char,"is a vowel")
+else:
+    print(char,"is consonant")
