cee65b198cf2a7c4aa76056faceea8b0aa610329
unknown <kpnagajothi1999@gmail.com>
2019-07-08 11:18:36 +0530

    'A.P'

---
+++ arith.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the 1st number of A.P :"))
+n=int(input("Enter the total number of A.P :"))
+d=int(input("Enter the comman difference  :"))
+total=(n*(2*a+(n-1)*d))/2
+tn=a+(n-1)*d
+print(" the sum of A.P=",total)
+print("the tn term of A.P=",tn)
+
+
