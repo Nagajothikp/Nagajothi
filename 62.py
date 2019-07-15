6eed661321d3615580b38dcb8d93b3b05b83afcf
unknown <kpnagajothi1999@gmail.com>
2019-07-15 13:48:49 +0530

    '62'

--- 62.py
+++ 62.py
@@ -1,10 +1,15 @@
-s=input()
-s1=set(s)
-if (s1=={"1","0"}):
-    print("yes")
-else:
-    print("NO")
-
-
+def check(string) : 
+    p = set(string) 
+    s = {'0', '1'} 
+    if s == p or p == {'0'} or p == {'1'}: 
+        print("Yes") 
+    else : 
+        print("No") 
+   
+if __name__ == "__main__" : 
+  
+    string = "1010100001122"
+   
+    check(string) 
 
 
