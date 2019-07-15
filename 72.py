db4ee04565215c9832d08719f2ee41a0ee7d725c
unknown <kpnagajothi1999@gmail.com>
2019-07-15 22:47:19 +0530

    '72'

---
+++ 72.py
@@ -0,0 +1,8 @@
+
+ch=input("Enter a char:")
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+ or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print(ch,"yes its a vowel" )
+else:
+    print(ch," its not a vowel")
+
