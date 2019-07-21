ea9caf25a6f37f0b036400bb827aed219eaecf15
unknown <kpnagajothi1999@gmail.com>
2019-07-21 13:04:31 +0530

    's11'

---
+++ s11.py
@@ -0,0 +1,7 @@
+x=["Monday","Tuesday","Wednesday","Thursday","Friday"]
+y=["saturday","sunday"]
+z=input("Enter a day:")
+if z!=y:
+    print("it's a working day")
+else:
+    print("yes,its a weekend")
