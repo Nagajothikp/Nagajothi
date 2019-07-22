164bff97919f499cdff4bdd69511585e565f4155
unknown <kpnagajothi1999@gmail.com>
2019-07-22 22:27:35 +0530

    's21'

---
+++ s21.py
@@ -0,0 +1,7 @@
+s,r=list(map(int,input("ENter a num:")))
+t,o=list(map(int,input("ENter a num:")))
+p,q=list(map(int,input("ENter a num:")))
+if s==r or t==0 or p==q or s==t==p or r==0==p:
+    print("Yes")
+else:
+    print("No")
