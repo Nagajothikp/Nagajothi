2354e45995e7933d992fcc8e363f4a479e21372d
unknown <kpnagajothi1999@gmail.com>
2019-07-22 20:12:24 +0530

    's17'

---
+++ s17.py
@@ -0,0 +1,9 @@
+l=int(input("Enter  the num:"))
+r=int(input("Enter  the num:"))
+num = 1
+while(num>0):
+    if((num%1)==0 and (num%r)==0):
+        print(num)
+        num = 0
+    else:
+        num+=1
