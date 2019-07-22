d2131a3d9990ff6bbb1c788b002d3d937a197225
unknown <kpnagajothi1999@gmail.com>
2019-07-22 11:54:30 +0530

    's15'

---
+++ s15.py
@@ -0,0 +1,14 @@
+from collections import Counter 
+  
+def find_most_occ_char(input): 
+  
+    wc = Counter(input) 
+  
+    s = max(wc.values()) 
+    i = wc.values().index(s) 
+      
+    print wc.items()[i] 
+  
+if __name__ == "__main__": 
+    input = 'geeksforgeeks'
+    find_most_occ_char(input) 
