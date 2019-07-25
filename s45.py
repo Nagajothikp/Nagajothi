f52de06ce3a260bf3604b26ebc9c829713c4c4ee
unknown <kpnagajothi1999@gmail.com>
2019-07-25 10:43:46 +0530

    's45'

---
+++ s45.py
@@ -0,0 +1,8 @@
+
+length = float(input('Please Enter the Length of a Triangle: '))
+width = float(input('Please Enter the Width of a Triangle: '))
+
+
+perimeter = 2 * (length + width)
+
+print("Perimeter of a Rectangle using", length, "and", width, " = ", perimeter)
