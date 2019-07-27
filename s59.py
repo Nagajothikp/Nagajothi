854f428e111950c1143519967d30b35eb044ace1
unknown <kpnagajothi1999@gmail.com>
2019-07-27 10:36:13 +0530

    's59'

---
+++ s59.py
@@ -0,0 +1,25 @@
+def isDigitPresent(x, d): 
+  
+    while (x > 0): 
+      
+        if (x % 10 == d): 
+            break
+  
+        x = x / 10
+      
+  
+    return (x > 0) 
+  
+  
+def printNumbers(n, d): 
+  
+    for i in range(0, n+1): 
+  
+        if (i == d or isDigitPresent(i, d)): 
+            print(i,end=" ") 
+  
+
+n = 47
+d = 7
+print("The number of values are") 
+printNumbers(n, d) 
