5e196800e6b9771b83aa0a05b2d7d3920a4b34f8
unknown <kpnagajothi1999@gmail.com>
2019-07-29 14:10:45 +0530

    's132'

--- s132.py
+++ s132.py
@@ -1,2 +1,26 @@
-array=[7,10,11,15,20,2,13]
-print(max(array),min(array))  
+def rearrange(arr, n): 
+    temp = n*[None] 
+    small,large =0,n-1
+  
+    flag = True
+  
+    for i in range(n): 
+        if flag is True: 
+            temp[i] = arr[large] 
+            large -= 1
+        else: 
+            temp[i] = arr[small] 
+            small += 1
+  
+        flag = bool(1-flag) 
+  
+    for i in range(n): 
+        arr[i] = temp[i] 
+    return arr 
+  
+arr = [1, 2, 3, 4, 5, 6] 
+n = len(arr) 
+print("Original Array") 
+print(arr) 
+print("Modified Array") 
+print(rearrange(arr, n)) 
