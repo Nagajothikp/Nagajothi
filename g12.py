5d29d850cee1aa4a65c767e2ce91f2f67965b34e
unknown <kpnagajothi1999@gmail.com>
2019-08-01 21:15:27 +0530

    'g12'

---
+++ g12.py
@@ -0,0 +1,8 @@
+n=input("ENter a num:")
+y=input("ENter a num:")
+n,y.sort()
+if __name__=='__main__':
+    arr=[n,y]
+    n=len(arr)
+    y=2
+    print("yth smallest element is",ythsmallest(arr,n,y))
