50008d05f400cda876c057016111ad7169a1c1ec
unknown <kpnagajothi1999@gmail.com>
2019-08-03 12:10:47 +0530

    'g29'

---
+++ g29.py
@@ -0,0 +1,7 @@
+s,r=list(input("Enter a number:"))
+t,q=list(input("Enter a number:"))
+p,q=list(input("Enter a number:"))
+if s==r or t==o or p==q or s==t==p or r==o==q:
+    print("yes")
+else:
+    print("no")
